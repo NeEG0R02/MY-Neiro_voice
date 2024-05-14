eba778cfd6aac09590fdbc84241b298be575ff77
Егор <157903234+NeEG0R02@users.noreply.github.com>
2024-05-14 20:40:08 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,16 @@
+TELEGRAM_TOKEN = '7092767373:AAElTkFo5_NXgLz3iIfcgNfaH4fkesy43uk'
+FOLDER_ID = 'b1ge3ntps6iq71kjdlr3'
+IAM_TOKEN = ('t1.9euelZrGnY-NlsaRx5qcz46Py8qVne3rnpWakpGZno-Lj43Ol5eRjJuVxpPl8_c5QW5P-e8gKhgB_d3z93lva0_57yAqGAH9zef1656VmpuVjMicjpSci46Nj5SenJjJ7_zF656VmpuVjMicjpSci46Nj5SenJjJveuelZrKzpXJx5vHiY3Mz5WPk56VibXehpzRnJCSj4qLmtGLmdKckJKPioua0pKai56bnoue0oye.aaEPWnunR2ou3WT5svDWLp8G5_kUXQENdmS-SlZBMv3jW71L4qheusVvdpDG2Orme8XpzH7XdTLmLgIIPsGjBA')
+COUNT_LAST_MSG = 4
+MAX_USER_TTS_SYMBOLS = 1000
+MAX_TTS_SYMBOLS = 200
+MAX_USER_STT_BLOCKS = 12
+MAX_USERS = 3
+MAX_GPT_TOKENS = 120
+MAX_USER_GPT_TOKENS = 2_000
+SYSTEM_PROMPT = [{'role': 'system',
+                  'text': 'Ты дружелюбный собеседник. '
+                          'Общайся с пользователем на "ты" и используй юмор.'
+                          'Поддерживай диалог. Изображай человека'}]
+LOGS = 'logs.txt'
+PATH_TO_DB = 'messages.db'
